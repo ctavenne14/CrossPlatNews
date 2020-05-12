e8e5d96fc0116b27085b7e80b6be98d1ed16d8e6
Cody Tavenner <ctavenne@asu.edu>
2020-05-11 19:38:25 -0700

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBzpQgcmI9QTgqJct2Xxj0Euo5b8lWfTDk",
+    authDomain: "crossplatnews.firebaseapp.com",
+    databaseURL: "https://crossplatnews.firebaseio.com",
+    projectId: "crossplatnews",
+    storageBucket: "crossplatnews.appspot.com",
+    messagingSenderId: "170635298803",
+    appId: "1:170635298803:web:6646d13e34d541fd5fb486"
+  };
+export default firebaseConfig;
