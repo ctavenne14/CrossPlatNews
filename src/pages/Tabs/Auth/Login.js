612be568bf4205960375c72aed3668752f7467a5
Cody Tavenner <ctavenne@asu.edu>
2020-05-12 19:12:21 -0700

    Validation for sign up, login, and password reset

--- src/pages/Tabs/Auth/Login.js
+++ src/pages/Tabs/Auth/Login.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import { IonPage, IonContent, IonItem, IonLabel, IonInput, IonRow, IonCol, IonButton, IonRouterLink } from '@ionic/react';
+import { IonPage, IonContent, IonItem, IonLabel, IonInput, IonRow, IonCol, IonButton, IonRouterLink, IonLoading } from '@ionic/react';
 import NavHeader from "../../../components/Headers/NavHeader";
 
-const Login = () => {
+import {toast} from "../../../helpers/toast";
+import useForm from '../../../hooks/UseForm';
+import validateLogin from '../../../validators/validateLogin';
+import firebase from '../../../firebase';
+
+const INITIAL_STATE = {
+    email: "",
+    password: "",
+}
+
+const Login = (props) => {
+
+    const {handleSubmit, handleChange, values, isSubmitting} = useForm(INITIAL_STATE, validateLogin, authenticatedUser)
+    const [busy, setBusy] = React.useState(false);
+
+    async function authenticatedUser() {
+        setBusy(true);
+        const {email, password} = values;
+        try{
+            await firebase.login(email, password);
+            toast("You have logged in successfully!");
+            props.history.push("/");
+        } catch (err) {
+            console.error("Authentication Error", err);
+            toast(err.message);
+        }
+        setBusy(false);
+    }
+
     return(
         <IonPage>
             <NavHeader title="Log In"/>
+            <IonLoading message="Please wait..." isOpen={busy} />
             <IonContent>
                 <IonItem lines="full">
                     <IonLabel postion="floating">Email</IonLabel>
-                    <IonInput name="email" type="text" required></IonInput>
+                    <IonInput name="email" type="text" value = {values.email} onIonChange = {handleChange} required></IonInput>
                 </IonItem>
                 <IonItem lines="full">
                     <IonLabel postion="floating">Password</IonLabel>
-                    <IonInput name="password" type="password" required></IonInput>
+                    <IonInput name="password" type="password" value = {values.password} onIonChange = {handleChange} required></IonInput>
                 </IonItem>
                 <IonRow>
                     <IonCol>
                         <IonButton 
                         type="submit"
                         color="primary"
-                        expand="block">
+                        expand="block" onClick={handleSubmit} disabled={isSubmitting} >
                             Log In
                         </IonButton>
                     </IonCol>
--- src/pages/Tabs/Auth/SignUp.js
+++ src/pages/Tabs/Auth/SignUp.js
@@ -1,30 +1,58 @@
 import React from 'react';
-import { IonPage, IonContent, IonItem, IonLabel, IonInput, IonRow, IonCol, IonButton } from '@ionic/react';
+import { IonPage, IonContent, IonItem, IonLabel, IonInput, IonRow, IonCol, IonButton, IonLoading } from '@ionic/react';
 import NavHeader from "../../../components/Headers/NavHeader";
+import {toast} from "../../../helpers/toast";
+import useForm from '../../../hooks/UseForm';
+import validateSignUp from '../../../validators/validateSignUp';
+import firebase from '../../../firebase';
 
-const SignUp = () => {
+const INITIAL_STATE = {
+    name: "",
+    email: "",
+    password: "",
+}
+
+const SignUp = (props) => {
+    const {handleSubmit, handleChange, values, isSubmitting} = useForm(INITIAL_STATE, validateSignUp, authenticatedUser)
+    const [busy, setBusy] = React.useState(false);
+
+    async function authenticatedUser() {
+        setBusy(true);
+        const {name, email, password} = values;
+        try{
+            await firebase.register(name, email, password);
+            toast("You have signed up successfully!");
+            props.history.push("/");
+        } catch (err) {
+            console.error("Authentication Error", err);
+            toast(err.message);
+        }
+        setBusy(false);
+    }
+
     return(
         <IonPage>
             <NavHeader title="Sign Up"/>
+            <IonLoading message="Please wait..." isOpen={busy} />
             <IonContent>
                 <IonItem lines="full">
                     <IonLabel postion="floating">Username</IonLabel>
-                    <IonInput name="name" type="text" required></IonInput>
+                    <IonInput name="name" type="text" value = {values.name} onIonChange = {handleChange} required></IonInput>
                 </IonItem>
                 <IonItem lines="full">
                     <IonLabel postion="floating">Email</IonLabel>
-                    <IonInput name="email" type="text" required></IonInput>
+                    <IonInput name="email" type="text" value = {values.email} onIonChange = {handleChange} required></IonInput>
                 </IonItem>
                 <IonItem lines="full">
                     <IonLabel postion="floating">Password</IonLabel>
-                    <IonInput name="password" type="password" required></IonInput>
+                    <IonInput name="password" type="password" value = {values.password} onIonChange = {handleChange} required></IonInput>
                 </IonItem>
                 <IonRow>
                     <IonCol>
                         <IonButton 
                         type="submit"
                         color="primary"
-                        expand="block">
+                        expand="block" onClick={handleSubmit} disabled={isSubmitting}>
                             Sign Up
                         </IonButton>
                     </IonCol>
