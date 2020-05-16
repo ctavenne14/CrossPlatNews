f9f29af5e6212a0ab5d76e4c83fef6e57847e876
Cody Tavenner <ctavenne@asu.edu>
2020-05-15 18:44:33 -0700

    Link Items for news and trending

--- src/pages/Tabs/News.js
+++ src/pages/Tabs/News.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { IonPage, IonContent} from '@ionic/react';
-import SmallHeader from '../../components/Headers/SmallHeader'
-import LargeHeader from '../../components/Headers/LargeHeader'
+import React from "react";
+import { IonPage, IonContent } from "@ionic/react";
+import SmallHeader from "../../components/Headers/SmallHeader";
+import LargeHeader from "../../components/Headers/LargeHeader";
+import LinkList from "../../Link/LinkList";
 
-const News = () => {
-    return (
-        <IonPage>
-            <SmallHeader title="News" />
-            <IonContent fullscreen>
-                <LargeHeader title="News"/>
-            </IonContent>
-        </IonPage>
-    )
-}
-export default News; 
+const News = (props) => {
+  return (
+    <IonPage>
+      <SmallHeader title="News" />
+      <IonContent fullscreen>
+        <LargeHeader title="News" />
+        <LinkList location={props.location} />
+      </IonContent>
+    </IonPage>
+  );
+};
+export default News;
--- src/pages/Tabs/Trending.js
+++ src/pages/Tabs/Trending.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { IonPage, IonContent} from '@ionic/react';
-import SmallHeader from '../../components/Headers/SmallHeader'
-import LargeHeader from '../../components/Headers/LargeHeader'
+import React from "react";
+import { IonPage, IonContent } from "@ionic/react";
+import SmallHeader from "../../components/Headers/SmallHeader";
+import LargeHeader from "../../components/Headers/LargeHeader";
+import LinkList from "../../Link/LinkList";
 
-const Trending = () => {
-    return (
-        <IonPage>
-            <SmallHeader title="Trending" />
-            <IonContent fullscreen>
-                <LargeHeader title="Trending"/>
-            </IonContent>
-        </IonPage>
-    )
-}
+const Trending = (props) => {
+  return (
+    <IonPage>
+      <SmallHeader title="Trending" />
+      <IonContent fullscreen>
+        <LargeHeader title="Trending" />
+        <LinkList location={props.location} />
+      </IonContent>
+    </IonPage>
+  );
+};
 export default Trending;
